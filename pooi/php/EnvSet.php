ab1a29eb535176f1aa426d75df5fca47000aad0d
RyuAoni <arrpst1158@gmail.com>
2025-11-13 17:34:35 +0900

    all

--- pooi/php/EnvSet.php
+++ pooi/php/EnvSet.php
@@ -2,9 +2,9 @@
 // EnvSet.php
 $_ENV['DB_DRIVER'] = 'mysql';
 $_ENV['DB_HOST'] = 'localhost';
-$_ENV['DB_PORT'] = '3306';
-$_ENV['DB_NAME'] = 'pooi';
+$_ENV['DB_PORT'] = 'port';
+$_ENV['DB_NAME'] = 'name';
 $_ENV['DB_CHARSET'] = 'utf8';
-$_ENV['PARKING_AREA'] = 'croudOne95'; // password
-$_ENV['USER_NAME'] = 'datakun';
+$_ENV['PARKING_AREA'] = 'pass'; // password
+$_ENV['USER_NAME'] = 'user_name';
 ?>
